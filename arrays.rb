d4dcb9e69813404c55aca8a37fe4e081cce3a9ac
ben <bjelinek42@gmail.com>
2022-03-14 16:12:37 -0600

    arrays practice

---
+++ arrays.rb
@@ -0,0 +1,41 @@
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+# words = ["man", "woman", "person"]
+# words << "dog" << "cat"
+# p words
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# letters = ["a", "b", "c", "d"]
+# letters[1] = 2
+# p letters
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.each do |number|
+#   p number
+# end
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+# numbers = [1]
+# numbers << 2 << 3 << 4
+# p numbers
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# words = ["man", "woman", "person"]
+# words[2] = words[2].upcase
+# p words
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
