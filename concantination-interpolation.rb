a849737b5bd459579d7a98fb5c3d1de199403f6c
ben <bjelinek42@gmail.com>
2022-03-14 09:34:26 -0600

    complete concant/interpolation practice

--- concantination-interpolation.rb
+++ concantination-interpolation.rb
@@ -36,10 +36,37 @@
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+# p "Enter a name."
+# name = gets.chomp
+# if name != "Santa"
+#   p "You're not Santa."
+# end
+
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# n = "The Hitchhiker's Guide to the Galaxy"
+# n2 = "Douglas Adams"
+# p n2 + " wrote " + n + "."
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+# n = "The Hitchhiker's Guide to the Galaxy"
+# n2 = "Douglas Adams"
+# p "#{n2} wrote #{n}."
+
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+# p "enter a password."
+# pass = gets.chomp
+# if pass == "Joshua"
+#   p "Shall we play a game?"
+# else
+#   p "Access denied"
+# end
+
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+c1 = "Chicago"
+c2 = "New York City"
+c3 = "Los Angeles"
+p "Three large cities are " + c1 + ", " + c2 + ", and " + c3 + "."
