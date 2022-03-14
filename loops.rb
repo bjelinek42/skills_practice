087dde36a584691c36987091dc2cba506a46213b
ben <bjelinek42@gmail.com>
2022-03-14 15:21:46 -0600

    complete loops practice

--- loops.rb
+++ loops.rb
@@ -34,18 +34,53 @@
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
-n = 1
-while n <= 10
-  p 9000
-  n += 1
-end
+# n = 1
+# while n <= 10
+#   p 9000
+#   n += 1
+# end
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
+# while true
+#   puts "Enter a number"
+#   num = gets.chomp
+#   if num.to_i > 10
+#     break
+#   end
+# end
+
 # 7. Write a while loop that prints the numbers 50 to 70.
+
+# n = 50
+# while n <= 70
+#   p n
+#   n += 1
+# end
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
+# n = 1
+# while n <= 144
+#   p 'Around the world'
+#   n += 1
+# end
+
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
+# while true
+#   puts "Enter a word"
+#   word = gets.chomp
+#   if word.length > 5
+#     break
+#   end
+# end
+
+
 # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+n = 2
+while n <= 40
+  p n
+  n += 2
+end
