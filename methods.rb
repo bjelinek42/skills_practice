39c595deff266f97a96e68bf42bcd33f9539dc64
ben <bjelinek42@gmail.com>
2022-03-14 09:51:55 -0600

    complete

--- methods.rb
+++ methods.rb
@@ -28,16 +28,47 @@
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
-p "enter a number"
-n = gets.chomp
-p n.to_i * 10
+# p "enter a number"
+# n = gets.chomp
+# p n.to_i * 10
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
+# p "enter a word"
+# n = gets.chomp
+# p "enter a word"
+# n2 = gets.chomp
+# p n.upcase + " " + n2.upcase
+
+
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# p "enter a word"
+# w = gets.chomp
+# p w.length
+
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+# p "enter a number"
+# n = gets.chomp
+# if n.to_i < 0
+#   p "That's a negative number"
+# end
+
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+# p "enter a number"
+# n = gets.chomp
+# p "enter a number"
+# n2 = gets.chomp
+
+# p n.to_i * n2.to_i
+
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+p "enter a word"
+word = gets.chomp
+if word.length > 5
+  p "that's a long word"
+end
