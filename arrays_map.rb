703a20434d9c89b23609df7f677fea5a4ffd25e3
ben <bjelinek42@gmail.com>
2022-03-15 16:20:05 -0600

    complete arrays map practice with each loops

--- arrays_map.rb
+++ arrays_map.rb
@@ -41,24 +41,59 @@
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
-strings = ["hello", "goodbye"]
-str_length = []
-strings.each do |str|
-  str_length << str.length
-end
-p str_length
+# strings = ["hello", "goodbye"]
+# str_length = []
+# strings.each do |str|
+#   str_length << str.length
+# end
+# p str_length
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+# people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+# ages = []
+# people.each do |person|
+#   ages << person[:age]
+# end
+# p ages
+
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+numbers = [1, 2, 3]
+half = []
+numbers.each do |num|
+  half << (num.to_f/2)
+end
+p half
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+# strings = ["hello", "goodbye"]
+# letters = []
+# strings.each do |str|
+#   letters << str[0]
+# end
+# p letters
+
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+# people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+# ages = []
+# people.each do |person|
+#   ages << person[:age] * 2
+# end
+# p ages
+
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+# numbers = [1, 2, 3]
+# str = []
+# numbers.each do |number|
+#   str << number.to_s
+# end
+# p str
