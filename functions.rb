0a9c5ac203f64fa2d45b07e33399725fce1cf853
ben <bjelinek42@gmail.com>
2022-03-15 15:36:05 -0600

    functions practice

---
+++ functions.rb
@@ -0,0 +1,44 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+# def double(x)
+#    x * 2
+# end
+# p double(4)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def capitalize(str)
+#   str.upcase
+# end
+# p capitalize("string")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+# def subtract(x, y)
+#   x - y
+# end
+# p subtract(10, 5)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+# def square(x)
+#   x * x
+# end
+# p square(10)
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+# def first_letter(str)
+#   str[0]
+# end
+# p first_letter("string")
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
