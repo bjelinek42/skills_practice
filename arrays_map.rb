c1c22f1e86239aed674d59fdcd4727b8d4d7edb8
ben <bjelinek42@gmail.com>
2022-03-15 15:54:07 -0600

    arrays map practice w/ each loop

---
+++ arrays_map.rb
@@ -0,0 +1,64 @@
+#  1. Start with an array of numbers and create a new array with each number times 3.
+#     For example, [1, 2, 3] becomes [3, 6, 9].
+
+# numbers = [1, 2, 3]
+# times_three = []
+# numbers.each do |number|
+#   times_three << number * 3
+# end
+# p times_three
+
+#  2. Start with an array of strings and create a new array with each string upcased.
+#     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+# strs = ["hello", "goodbye"]
+# capital = []
+# strs.each do |str|
+#   capital << str.upcase
+# end
+# p capital
+
+#  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+#     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+# people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+# names = []
+# people.each do |person|
+#   names << person[:name]
+# end
+# p names
+
+#  4. Start with an array of numbers and create a new array with each number plus 7.
+#     For example, [1, 2, 3] becomes [8, 9, 10].
+
+# numbers = [1, 2, 3]
+# more = []
+# numbers.each do |number|
+#   more << number + 7
+# end
+# p more
+
+#  5. Start with an array of strings and create a new array with each string's length.
+#     For example, ["hello", "goodbye"] becomes [5, 7].
+
+strings = ["hello", "goodbye"]
+str_length = []
+strings.each do |str|
+  str_length << str.length
+end
+p str_length
+
+#  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
+#     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+#  7. Start with an array of numbers and create a new array with each number divided by 2.
+#     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+#  8. Start with an array of strings and create a new array with each string's first letter only.
+#     For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+# 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+#     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+# 10. Start with an array of numbers and create a new array with each number converted into a string.
+#     For example, [1, 2, 3] becomes ["1", "2", "3"].
