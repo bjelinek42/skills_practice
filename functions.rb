78c2fd73fb94688868009680be5294d9b0c85b84
ben <bjelinek42@gmail.com>
2022-03-15 15:42:45 -0600

    complete functions practice

--- functions.rb
+++ functions.rb
@@ -35,10 +35,35 @@
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def combo(x, y, z)
+#   "#{x} #{y} #{z}"
+# end
+# p combo("peas", "carrots", "potatoes")
+
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# def str(num)
+#   num.to_s
+# end
+# p str(9876)
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
+# def five(str)
+#   str * 5
+# end
+# p five("string")
+
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def average(x, y, z)
+#   (x + y + z)/3
+# end
+# p average(4, 8, 12)
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def add(n)
+  n * 10 + 30
+end
+p add(10)
