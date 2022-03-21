201ad41ab6e37a458cddd1972edcbdff71a1e755
ben <bjelinek42@gmail.com>
2022-03-21 09:16:10 -0600

    finish arrrays reduce practic

--- arrays_reduce.rb
+++ arrays_reduce.rb
@@ -53,14 +53,55 @@
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
+# products = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# lowest_price = products[0][:price]
+# products.each do |product|
+#   if product[:price] < lowest_price
+#     lowest_price = product[:price]
+#   end
+# end
+# p lowest_price
+
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
+
+# numbers = [5, 10, 8, 3]
+# product = 1
+# numbers.each do |number|
+#   product = product * number
+# end
+# p product
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+# strings = ["volleyball", "basketball", "badminton"]
+# string = "-"
+# strings.each do |str|
+#   string = string + str + "-"
+# end
+# p string
+
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
+# products = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# shortest_name = products[0]
+# products.each do |product|
+#   if product[:name].length < shortest_name[:name].length
+#     shortest_name = product
+#   end
+# end
+# p shortest_name
+
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
+
+# numbers = [5, 10, 8, 3]
+# max_num = numbers[0]
+# numbers.each do |number|
+#   if number > max_num
+#     max_num = number
+#   end
+# end
+# p max_num
