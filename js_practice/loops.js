c66fc223c2077b1c91eb108603dbcb7be228ad45
ben <bjelinek42@gmail.com>
2022-03-30 09:15:12 -0600

    js loops practice

---
+++ js_practice/loops.js
@@ -0,0 +1,61 @@
+// # 1. Write a while loop to print the numbers 1 through 10.
+
+// var n = 0;
+// while (n < 10) {
+//   ++n;
+//   console.log(n);
+// }
+
+// # 2. Write a while loop that prints the word "hello" 5 times.
+
+// var n = 0;
+// while (n < 5) {
+//   console.log('hello');
+//   n++;
+// }
+
+// # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+// var n = 0;
+// while (n <= 100) {
+//   console.log(n);
+//   n += 5;
+// }
+
+// # 5. Write a while loop that prints the number 9000 ten times.
+
+// var n = 0;
+// while (n < 10) {
+//   console.log(9000);
+//   n++;
+// }
+
+// # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// # 7. Write a while loop that prints the numbers 50 to 70.
+
+// var n = 50;
+// while (n <= 70) {
+//   console.log(n);
+//   n++
+// }
+
+// # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+// var n = 0;
+// while (n <= 144) {
+//   console.log('Around the World');
+//   n++;
+// }
+
+// # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+// # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+// var n = 2;
+// while (n <= 40) {
+//   console.log(n);
+//   n += 2;
+// }
