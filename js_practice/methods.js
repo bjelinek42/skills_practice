17a7dc982dc0733ffd99789e893c279ed21d0d15
ben <bjelinek42@gmail.com>
2022-03-30 08:56:25 -0600

    js methods

---
+++ js_practice/methods.js
@@ -0,0 +1,62 @@
+// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+// var word = "pencil";
+// console.log(word.toUpperCase());
+
+// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// var n = 199;
+// if ( n > 100) {
+//   console.log("That's a big number!");
+// }
+
+// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+// var n = 5;
+// var m = 9;
+// console.log(n + m);
+
+// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+// var word = 'pencil';
+// var split = word.split("");
+// var reverse = split.reverse();
+// var join = reverse.join("");
+// console.log(join);
+
+
+// # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+// var n = 5;
+// console.log(n * 10);
+
+// # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+// var w1 = 'pencil';
+// var w2 = 'paper';
+// console.log(`${w1.toUpperCase()} ${w2.toUpperCase()}`);
+
+// # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// var word = 'pencil';
+// console.log(word.length);
+
+// # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// var n = -8;
+// if (n < 0) {
+//   console.log("That's a negative number");
+// }
+
+// # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+// var n = 9;
+// var m = 5;
+// console.log(n * m);
+
+// # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+// var word = 'pencil';
+// if (word.length > 5) {
+//   console.log("Thats a long word");
+// }
