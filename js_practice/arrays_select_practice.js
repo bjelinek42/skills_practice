b919ff71725f59d681ee2279e6c8cd39f791e0a4
ben <bjelinek42@gmail.com>
2022-04-11 09:37:32 -0600

    array select practice

---
+++ js_practice/arrays_select_practice.js
@@ -0,0 +1,74 @@
+// #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
+// #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+// var numbers = [2, 32, 80, 18, 12, 3];
+// var lowNumbers = [];
+// numbers.forEach(number => {
+//   if (number < 20) {
+//     lowNumbers.push(number);
+//   }
+// });
+// console.log(lowNumbers)
+
+// #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+// #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+// var strings = ["winner", "winner", "chicken", "dinner"];
+// var wStrings = [];
+// strings.forEach(string => {
+//   if (string[0] === 'w') {
+//     wStrings.push(string);
+//   }
+// });
+// console.log(wStrings);
+
+// #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+// var products = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
+// var highPrices = [];
+// products.forEach(product => {
+//   if (product.price > 5) {
+//     highPrices.push(product);
+//   }
+// });
+// console.log(highPrices)
+
+// #  4. Start with an array of numbers and create a new array with only the even numbers.
+// #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+// var numbers = [2, 4, 5, 1, 8, 9, 7];
+// var evens = [];
+// numbers.forEach(number => {
+//   if (number % 2 === 0) {
+//     evens.push(number);
+//   }
+// });
+// console.log(evens);
+
+// #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+// #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+// var strings = ["a", "man", "a", "plan", "a", "canal", "panama"];
+// var shortStrings = [];
+// strings.forEach(str => {
+//   if (str.length < 4) {
+//     shortStrings.push(str);
+//   }
+// })
+// console.log(shortStrings);
+
+// #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+// #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+// #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+// #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+// #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+// #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+// # 10. Start with an array of numbers and create a new array with only the odd numbers.
+// #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
