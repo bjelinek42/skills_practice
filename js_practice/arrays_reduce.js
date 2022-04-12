aaad70f5ccc1f65560e8b07c2875ed99ae7827e0
ben <bjelinek42@gmail.com>
2022-04-12 09:12:45 -0600

    arrays reduce practice js

---
+++ js_practice/arrays_reduce.js
@@ -0,0 +1,107 @@
+// #  1. Start with an array of numbers and compute the sum of all the numbers.
+// #     For example, [5, 10, 8, 3] becomes 26.
+
+// var numbers = [5, 10, 8, 3];
+// var sum = 0;
+// numbers.forEach(number => {
+//   sum += number
+// })
+// console.log(sum)
+
+// #  2. Start with an array of strings and combine them all into a single string.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+// var strings = ["volleyball", "basketball", "badminton"];
+// var string = '';
+// strings.forEach(str => {
+//   string += str;
+// })
+// console.log(string);
+
+// #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+// var products = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
+// var sum = 0;
+// products.forEach(product => {
+//   sum += product.price;
+// })
+// console.log(sum);
+
+// #  4. Start with an array of numbers and compute the the minumum number.
+// #     For example, [5, 10, 8, 3, 9] becomes 3.
+
+// var numbers = [5, 10, 8, 3, 9];
+// var minNumber = numbers[0];
+// numbers.forEach(number => {
+//   if (number < minNumber) {
+//     minNumber = number;
+//   }
+// });
+// console.log(minNumber);
+
+// #  5. Start with an array of strings and compute the total length of all the strings.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+// var strings = ["volleyball", "basketball", "badminton"];
+// var length = 0;
+// strings.forEach(str => {
+//   length += str.length;
+// });
+// console.log(length);
+
+// #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+// var products = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+// var lowestPrice = products[0]
+// products.forEach(product => {
+//   if (product.price < lowestPrice.price) {
+//     lowestPrice = product;
+//   }
+// });
+// console.log(lowestPrice);
+
+// #  7. Start with an array of numbers and compute product of all the numbers.
+// #     For example, [5, 10, 8, 3] becomes 1200.
+
+// var numbers = [5, 10, 8, 3];
+// var product = 1;
+// numbers.forEach(num => {
+//   product *= num;
+// });
+// console.log(product);
+
+// #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+// var strings = ["volleyball", "basketball", "badminton"];
+// var string = '-';
+// strings.forEach(str => {
+//   string += str + '-';
+// });
+// console.log(string);
+
+// #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+// var products = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
+// var shortestName = products[0];
+// products.forEach(product => {
+//   if (product.name.length < shortestName.name.length) {
+//     shortestName = product;
+//   }
+// });
+// console.log(shortestName);
+
+// # 10. Start with an array of numbers and compute the maximum number.
+// #     For example, [5, 10, 8, 3] becomes 10.
+
+// var numbers = [5, 10, 8, 3];
+// var maxNumber = numbers[0];
+// numbers.forEach(number => {
+//   if (maxNumber < number) {
+//     maxNumber = number;
+//   }
+// });
+// console.log(maxNumber);
